0b5d6f8d78b75f42ccee876508f3a0a7df89c12d
R Y <yohannan.r@husky.neu.edu>
2020-01-27 10:52:09 -0500

    Renamed gps_driver.py to driver.py to avoid package name collision

--- scripts/driver.py
+++ scripts/driver.py
@@ -2,18 +2,20 @@
 import rospy
 import utm
 from std_msgs.msg import Float64
+from gps_driver.msg import GNSS
 import random
 
 if __name__ == "__main__":
 	try:
 		rospy.init_node("gps_driver")
-		out = rospy.Publisher("/gps_out_meters", Float64, queue_size=10)
+		out = rospy.Publisher("/gps_out", GNSS, queue_size=10)
 		random.seed()
 		rate = rospy.Rate(10)
 		while not rospy.is_shutdown():
+			msg = GNSS()
 			reading = random.random()
 			rospy.loginfo("m = %.2f" % (reading))
-			out.publish(reading)
+			out.publish(msg)
 			rate.sleep()
 	except rospy.ROSInterruptException:
 		pass
